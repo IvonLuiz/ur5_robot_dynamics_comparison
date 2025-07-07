9268a94f047d0fb770d7e89eea1036e09d3cb6c6
IvonLuiz <ivonluizgarcia@gmail.com>
2025-07-07 17:23:33 -0300

    fix dynamics

--- ur5_dynamics.py
+++ ur5_dynamics.py
@@ -6,55 +6,54 @@ from scipy.integrate import odeint
 from spatialmath.base import trotz, transl
 
 class UR5DynamicModel:
-    def __init__(self):
-        # Parâmetros do UR5 (valores aproximados)
-        self.m = [3.7, 8.4, 2.33, 1.65, 1.25, 0.8]  # massas dos links (kg)
-        self.l = [0.425, 0.392, 0.1, 0.1, 0.1, 0.1]  # comprimentos (m)
-        self.r = [  # Centros de massa (relativos ao frame do link)
-            [0, 0, 0.1],
-            [0.2, 0, 0],
-            [0, 0, -0.05],
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0.02]
-        ]
-        
-        # Tensores de inércia (simplificados) [Ixx, Iyy, Izz] (kg.m^2)
-        self.I = [
-            [0.1, 0.1, 0.05],
-            [0.2, 0.2, 0.1],
-            [0.05, 0.05, 0.03],
-            [0.03, 0.03, 0.01],
-            [0.02, 0.02, 0.01],
-            [0.01, 0.01, 0.005]
-        ]
-        
-        # Limites das juntas
-        self.tau_max = np.array([150, 150, 150, 28, 28, 28])  # Torques máximos (Nm)
-        self.qlim = [
-            [-np.pi, np.pi],
-            [-np.pi, np.pi],
-            [-np.pi, np.pi],
-            [-np.pi, np.pi],
-            [-np.pi, np.pi],
-            [-np.pi, np.pi]
-        ]
-        
-        # Parâmetros DH (Denavit-Hartenberg)
+    def __init__(self, verbose=True):
+        # Center of mass of each link in its local frame (m)
+        self.mass = np.array([3.7, 8.393, 2.33, 1.219, 1.219, 0.1879]) # Mass (kg)
+        # Center of mass of each link in its local frame (m)
+        self.r = np.array([
+            [0.0, -0.02561, 0.00193], # L1
+            [0.2125, 0.0, 0.11336],   # L2
+            [0.15, 0.0, 0.0265],      # L3
+            [0.0, -0.0018, 0.01634],  # L4
+            [0.0, 0.0018, 0.01634],   # L5
+            [0.0, 0.0, -0.001159],    # L6
+        ])
+        # Principal inertia about the COM (diagonal only) [Ixx, Iyy, Izz] (kg.m^2)
+        self.I = np.array([
+            [0.010267, 0.010267, 0.00666],
+            [0.226890, 0.226890, 0.0151074],
+            [0.049443, 0.049443, 0.004095],
+            [0.111172, 0.111172, 0.21942],
+            [0.111172, 0.111172, 0.21942],
+            [0.017136, 0.017136, 0.033822],
+        ])
+        # Gravity acceleration vector (m/s²)
+        self.gravity = np.array([0, 0, -9.81])
+        # Denavit-Hartenberg (DH) Parameters [a, d, alpha, theta_offset]
         self.dh_params = [
-            [0, 0.089159, 0, np.pi/2],
+            [0, 0.089159, 0, np.pi / 2],
             [0, 0, -0.425, 0],
             [0, 0, -0.39225, 0],
-            [0, 0.10915, 0, np.pi/2],
-            [0, 0.09465, 0, -np.pi/2],
-            [0, 0.0823, 0, 0]
+            [0, 0.10915, 0, np.pi / 2],
+            [0, 0.09465, 0, -np.pi / 2],
+            [0, 0.0823, 0, 0],
         ]
+
+        self.l = [0.425, 0.392, 0.1, 0.1, 0.1, 0.1]  # lengths (m)
+
+        # Joint limits
+        self.tau_max = np.array([150, 150, 150, 28, 28, 28]) # (Nm)
         
-        # Gravidade
-        self.g = np.array([0, 0, -9.81])
-        
+        # Option to enable/disable logging
+        self.verbose = verbose
+
     def forward_kinematics(self, q):
-        """Calcula a cinemática direta para cada junta"""
+        '''!
+        Calculates forward kinematics for each link.
+
+        \param   q (np.ndarray): Joint positions (radians).
+        \return  List[SE3]: Homogeneous transforms from base to each link.
+        '''
         T = []
         current_T = SE3()
         
@@ -62,91 +61,111 @@ class UR5DynamicModel:
             a, d, alpha, theta = self.dh_params[i]
             theta += q[i]
             
-            Ti = SE3.Rz(theta) * SE3.Tz(d) * SE3.Tx(a) * SE3.Rx(alpha)
+            Ti = SE3.Rz(theta) * SE3.Tz(d) * SE3.Tx(a) * SE3.Rx(alpha) # link transform
             current_T = current_T * Ti
             T.append(current_T)
         
         return T
     
     def jacobian(self, q):
-        """Calcula o Jacobiano para cada centro de massa"""
+        '''!
+        Calculates the Jacobian for each center of mass.
+        
+        \param   q (np.ndarray): Joint positions (radians).
+        \return  List[Tuple[np.ndarray, np.ndarray]]: List of tuples containing linear and angular Jacobians for each link.
+        '''
         T = self.forward_kinematics(q)
         J = []
         
         for i in range(6):
-            # Posição do centro de massa no frame global
-            com_global = T[i] * SE3(transl(self.r[i]))
-            
-            # Jacobiano linear e angular
+            # Linear and angular Jacobian
             Jvi = np.zeros((3, 6))
             Jwi = np.zeros((3, 6))
+            p_com_global = (T[i] * SE3(transl(self.r[i]))).t
             
             for j in range(6):
                 if j <= i:
-                    # Eixo de rotação no frame global
+                    # rotational axis in global frame
                     z_axis = T[j].R[:, 2]
                     
-                    # Jacobiano angular
+                    # angular Jacobian
                     Jwi[:, j] = z_axis
                     
-                    # Jacobiano linear
-                    p_com = com_global.t - T[j].t
+                    # linear jacobian
+                    p_com = p_com_global - T[j].t
                     Jvi[:, j] = np.cross(z_axis, p_com)
-                else:
-                    Jwi[:, j] = 0
-                    Jvi[:, j] = 0
-            
-            J.append({'linear': Jvi, 'angular': Jwi})
-        
+
+            J.append((Jvi, Jwi))
+
         return J
     
     def inertia_matrix(self, q):
-        """Calcula a matriz de inércia D(q)"""
+        '''!
+        Calculates the manipulator's inertia matrix D(q) in joint space.
+
+        \param   q (np.ndarray): Joint positions (radians).
+        \return  np.ndarray: The 6x6 inertia matrix (D).
+        '''
         J = self.jacobian(q)
         D = np.zeros((6, 6))
         
         for i in range(6):
-            Jv = J[i]['linear']
-            Jw = J[i]['angular']
-            R = self.forward_kinematics(q)[i].R
+            Jv_i, Jw_i = J[i]
+            R_i = self.forward_kinematics(q)[i].R
             
-            # Tensor de inércia no frame global
-            I_global = R @ np.diag(self.I[i]) @ R.T
-            
-            D += self.m[i] * Jv.T @ Jv + Jw.T @ I_global @ Jw
-        
+            # Inertia tensor on global frame
+            I_global = R_i @ np.diag(self.I[i]) @ R_i.T
+
+            D += self.mass[i] * Jv_i.T @ Jv_i + Jw_i.T @ I_global @ Jw_i
+
         return D
-    
+
     def coriolis_matrix(self, q, qd):
-        """Calcula a matriz de Coriolis C(q, qd)"""
-        D = np.array(self.inertia_matrix(q))
+        '''!
+        Calculates the Coriolis and centrifugal forces matrix C(q, qd).
+
+        \param   q (np.ndarray): Joint positions (radians).
+        \param   qd (np.ndarray): Joint velocities (radians/s).
+        \param   D (np.ndarray, optional): Pre-calculated inertia matrix D(q).
+        \return  np.ndarray: The 6x6 Coriolis matrix (C).
+        '''
+        D = self.inertia_matrix(q)
         C = np.zeros((6, 6))
-        h = 1e-6  # Passo para diferenças finitas
+        h = 1e-6
 
         for k in range(6):
             for j in range(6):
                 for i in range(6):
-                    # Calcula derivadas parciais via diferenças finitas
-                    q_plus = q.copy()
-                    q_plus[i] += h
-                    D_plus = self.inertia_matrix(q_plus)
+                    # Calculate partial derivatives using finite differences
+                    dq_i = np.zeros(6)
+                    dq_i[i] = h
+                    D_plus = self.inertia_matrix(q + dq_i)
+                    D_minus = self.inertia_matrix(q - dq_i)
+                    dDkj_dqi = (D_plus[k,j] - D_minus[k,j]) / (2*h)
                     
-                    q_minus = q.copy()
-                    q_minus[i] -= h
-                    D_minus = self.inertia_matrix(q_minus)
+                    dq_j = np.zeros(6)
+                    dq_j[j] = h
+                    D_plus = self.inertia_matrix(q + dq_j)
+                    D_minus = self.inertia_matrix(q - dq_j)
+                    dDki_dqj = (D_plus[k,i] - D_minus[k,i]) / (2*h)
                     
-                    dDkj_qi = (D_plus[k, j] - D_minus[k, j]) / (2 * h)
-                    dDki_qj = (D_plus[k, i] - D_minus[k, i]) / (2 * h)
-                    dDij_qk = (D_plus[i, j] - D_minus[i, j]) / (2 * h)
+                    dq_k = np.zeros(6)
+                    dq_k[k] = h
+                    D_plus = self.inertia_matrix(q + dq_k)
+                    D_minus = self.inertia_matrix(q - dq_k)
+                    dDij_dqk = (D_plus[i,j] - D_minus[i,j]) / (2*h)
                     
-                    # Símbolos de Christoffel
-                    c_ijk = 0.5 * (dDkj_qi + dDki_qj - dDij_qk)
-                    C[k, j] += c_ijk * qd[i]
+                    C[k,j] += 0.5 * (dDkj_dqi + dDki_dqj - dDij_dqk) * qd[i]
         
         return C
     
     def gravity_vector(self, q):
-        """Calcula o vetor de gravidade G(q)"""
+        '''!
+        Calculates the gravity force vector G(q) in joint space.
+
+        \param   q (np.ndarray): Joint positions (radians).
+        \return  np.ndarray: The 6x1 gravity force vector (G).
+        '''
         T = self.forward_kinematics(q)
         G = np.zeros(6)
         
@@ -155,7 +174,7 @@ class UR5DynamicModel:
             com_global = T[i] * SE3(transl(self.r[i]))
             
             # Energia potencial
-            P = self.m[i] * self.g @ com_global.t
+            P = self.mass[i] * self.gravity @ com_global.t
             
             # Gradiente da energia potencial
             for j in range(6):
@@ -165,120 +184,101 @@ class UR5DynamicModel:
                 q_plus[j] += epsilon
                 T_plus = self.forward_kinematics(q_plus)
                 com_plus = T_plus[i] * SE3(transl(self.r[i]))
-                P_plus = self.m[i] * self.g @ com_plus.t
+                P_plus = self.mass[i] * self.gravity @ com_plus.t
                 
                 G[j] += (P_plus - P) / epsilon
         
         return G
     
     def friction(self, qd):
-        """Modelo de atrito simples"""
-        return 0.1 * qd  # Atrito viscoso
+        '''!
+        Calculates friction forces in joint space (viscous model).
+
+        \param   qd (np.ndarray): Joint velocities (radians/s).
+        \return  np.ndarray: The 6x1 friction force vector.
+        '''
+        return 0.05 * qd
     
-    def dynamics(self, y, t, tau):
-        """Equações dinâmicas para integração"""
-        q = y[:6]
-        qd = y[6:]
+    def simulate(self, q0, qd0, tau, t_span, verbose=False):
+        '''!
+        Simulates the robot dynamics using numerical integration with optional logging.
         
-        D = self.inertia_matrix(q)
-        C = self.coriolis_matrix(q, qd)
-        G = self.gravity_vector(q)
-        F = self.friction(qd)
-        
-        qdd = np.linalg.inv(D) @ (tau - C @ qd - G - F)
-        
-        return np.concatenate((qd, qdd))
-    
-    def simulate(self, q0, qd0, tau, t_span):
-        """Simula a dinâmica do robô"""
+        \param   q0 (np.ndarray): Initial joint positions (radians).
+        \param   qd0 (np.ndarray): Initial joint velocities (radians/s).
+        \param   tau (np.ndarray): Joint torques (Nm).
+        \param   t_span (np.ndarray): Time span for the simulation (seconds).
+        \param   verbose (bool): If True, prints simulation progress.
+        \return  np.ndarray: The state trajectory [q, qd] over time.
+        '''
         y0 = np.concatenate((q0, qd0))
-        sol = odeint(self.dynamics, y0, t_span, args=(tau,))
-        return sol
+        cache = {'last_q': None, 'last_D': None, 'last_C': None, 'last_G': None}
+        self.verbose = verbose
+        
+        def dynamics_with_logging(y, t, tau):
+            '''!
+            ODE function to compute the dynamics with logging.
+            dy/dt = [qd, qdd]
+            
+            \param   y (np.ndarray): State vector [q, qd].
+            \param   t (float): Current time.
+            \param   tau (np.ndarray): Joint torques (Nm).
+            \return  np.ndarray: The derivative dy/dt.
+            '''
+            q = y[:6]
+            qd = y[6:]
+            if self.verbose:
+                print(f"\rSimulating t = {t:.2f}s", end='', flush=True)
+            if cache['last_q'] is None or np.any(np.abs(q - cache['last_q']) > 1e-6):
+                D = self.inertia_matrix(q)
+                C = self.coriolis_matrix(q, qd)
+                G = self.gravity_vector(q)
+                cache.update({'last_q': q.copy(), 'last_D': D, 'last_C': C, 'last_G': G})
+            else:
+                D, C, G = cache['last_D'], cache['last_C'], cache['last_G']
+            F = self.friction(qd)
+            qdd = np.linalg.solve(D, (tau - C @ qd - G - F))
+            return np.concatenate((qd, qdd))
+        
+        if self.verbose:
+            print("...")
+        try:
+            sol = odeint(
+                dynamics_with_logging,
+                y0,
+                t_span,
+                args=(tau,),
+                rtol=1e-6,
+                atol=1e-8,
+                mxstep=1000
+            )
+            if self.verbose:
+                print("\nSimulação concluída com sucesso!")
+            return sol
+        except Exception as e:
+            print(f"\nErro durante a integração: {str(e)}")
+            raise
 
-def create_ur5_rtb():
-    """Cria o modelo do UR5 na Robotics Toolbox"""
-    # Parâmetros DH do UR5
-    return DHRobot([
-        RevoluteDH(d=0.089159, alpha=np.pi/2),
-        RevoluteDH(a=0.425),
-        RevoluteDH(a=0.39225),
-        RevoluteDH(d=0.10915, alpha=np.pi/2),
-        RevoluteDH(d=0.09465, alpha=-np.pi/2),
-        RevoluteDH(d=0.0823)
-    ], name="UR5")
-
-def compare_models():
-    """Compara nosso modelo com a Robotics Toolbox"""
-    # Configuração inicial
-    q0 = np.array([0.1, -0.2, 0.3, -0.4, 0.5, -0.6])
-    qd0 = np.zeros(6)
-    
-    # Torques aleatórios dentro dos limites
-    np.random.seed(42)
-    tau_max = np.array([150, 150, 150, 28, 28, 28])
-    tau = np.random.uniform(-0.5, 0.5, 6) * tau_max
-    
-    # Tempo de simulação
-    t_span = np.linspace(0, 5, 500)
-    
-    # Simulação com nosso modelo
-    ur5 = UR5DynamicModel()
-    sol_custom = ur5.simulate(q0, qd0, tau, t_span)
-    
-    # Simulação com Robotics Toolbox
-    ur5_rtb = create_ur5_rtb()
-    
-    # Para a Robotics Toolbox, precisamos definir parâmetros dinâmicos
-    # (usando os mesmos parâmetros do nosso modelo para comparação justa)
-    ur5_rtb.links[0].m = ur5.m[0]
-    ur5_rtb.links[1].m = ur5.m[1]
-    ur5_rtb.links[2].m = ur5.m[2]
-    ur5_rtb.links[3].m = ur5.m[3]
-    ur5_rtb.links[4].m = ur5.m[4]
-    ur5_rtb.links[5].m = ur5.m[5]
-    
-    ur5_rtb.links[0].r = ur5.r[0]
-    ur5_rtb.links[1].r = ur5.r[1]
-    ur5_rtb.links[2].r = ur5.r[2]
-    ur5_rtb.links[3].r = ur5.r[3]
-    ur5_rtb.links[4].r = ur5.r[4]
-    ur5_rtb.links[5].r = ur5.r[5]
-    
-    # Como a Robotics Toolbox não tem uma função direta para simulação dinâmica com torques,
-    # vamos usar a função fdyn que simula a dinâmica com controle de torque
-    def control(t, x):
-        return tau  # Aplicamos os mesmos torques constantes
-    
-    sol_rtb = ur5_rtb.fdyn(5, q0, qd0, control=control, dt=0.01)
-    
-    # Plotar resultados
-    plt.figure(figsize=(14, 10))
-    
-    # Posições das juntas
-    for i in range(6):
-        plt.subplot(3, 2, i+1)
-        plt.plot(t_span, sol_custom[:, i], label='Nosso Modelo')
-        plt.plot(sol_rtb.t, sol_rtb.q[:, i], '--', label='Robotics Toolbox')
-        plt.title(f'Junta {i+1}')
-        plt.xlabel('Tempo (s)')
-        plt.ylabel('Posição (rad)')
-        plt.legend()
-        plt.grid(True)
-    
-    plt.tight_layout()
-    plt.suptitle('Comparação entre Nosso Modelo e Robotics Toolbox', y=1.02)
-    plt.show()
-    
-    # Calcular e mostrar erros
-    q_custom = sol_custom[-1, :6]  # Posições finais do nosso modelo
-    q_rtb = sol_rtb.q[-1, :]       # Posições finais da Robotics Toolbox
-    
-    errors = q_custom - q_rtb
-    print("\nErros nas posições finais das juntas:")
-    for i in range(6):
-        print(f"Junta {i+1}: {errors[i]:.6f} rad")
-    
-    print(f"\nErro RMS: {np.sqrt(np.mean(errors**2)):.6f} rad")
 
 if __name__ == "__main__":
-    compare_models()
+    ur5_model = UR5DynamicModel()
+    q_example = np.array([0.1, 0.2, 0.0, 0.4, 0.0, 0.6])
+    qd_example = np.array([0.01, 0.02, 0.0, 0.04, 0.0, 0.06])
+
+    print("--- UR5 Dynamic Model Test ---")
+    T_links = ur5_model.forward_kinematics(q_example)
+    print(f"End-effector Transform:\n{T_links[5].t}\n")
+
+    J_links = ur5_model.jacobian(q_example)
+    print(f"Link 3 Linear Jacobian (first 3 rows):\n{J_links[2][0][:3, :3]}\n")
+
+    D_matrix = ur5_model.inertia_matrix(q_example)
+    print(f"Inertia Matrix (D) shape: {D_matrix.shape}\n")
+
+    C_matrix = ur5_model.coriolis_matrix(q_example, qd_example)
+    print(f"Coriolis Matrix (C) shape: {C_matrix.shape}\n")
+
+    G_vector = ur5_model.gravity_vector(q_example)
+    print(f"Gravity Vector (G) shape: {G_vector.shape}\n")
+
+    F_friction = ur5_model.friction(qd_example)
+    print(f"Friction Forces (F_friction) shape: {F_friction.shape}\n")
